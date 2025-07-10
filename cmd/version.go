a50a88b1804d1e3cc635c47fb1311bb4fde10b07
Pranav Tripathi <111558974+prnvtripathi@users.noreply.github.com>
2025-07-10 12:45:26 +0530

    fix: correct spelling of 'dep-tree' in version command

--- cmd/version.go
+++ cmd/version.go
@@ -11,10 +11,10 @@ const CliVersion = "v0.1.0"
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Know the installed version of deps-tree",
-	Long:  `This command will help you to know the installed version of deps-tree`,
+	Short: "Know the installed version of dep-tree",
+	Long:  `This command will help you to know the installed version of dep-tree`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("deps-tree version:", CliVersion, "\nTo check and update the latest version, run 'deps-tree update'")
+		fmt.Println("dep-tree version:", CliVersion, "\nTo check and update the latest version, run 'dep-tree update'")
 	},
 }
 
